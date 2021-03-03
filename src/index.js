0f7090d6d1016fe7f7cae73bede75c546155ad09
Ricardo <ricardoinliner@yahoo.com>
2021-03-03 10:22:29 -0300

    criando rotas
    criação de rotas com resultado hardcoded

--- src/index.js
+++ src/index.js
@@ -3,9 +3,33 @@ const express = require('express')
 const app = express()
 
 app.get("/projects", (req, res) => {
+  return res.json([
+    'Projeto 1',
+    'Projeto 2'
+  ])
+})
 
-  return res.json({ message: "Ola mundo"})
+app.post('/projects', (req, res) => {
+  return res.status(201).json([
+    'Projeto 1',
+    'Projeto 2',
+    'Projeto 3'
+  ])
+})
 
+app.put('/projects/:id', (req, res) => {
+  return res.json([
+    'Projeto 1',
+    'Projeto 22',
+    'Projeto 3'
+  ])
+})
+
+app.delete('/projects/:id', (req, res) => {
+  return res.json([
+    'Projeto 1',
+    'Projeto 3'
+  ])
 })
 
 app.listen(3333, () => console.log("👻 - Back-end started"))
